18be76279d2fb73355c4b3e7c8e200ef34143381
Clay Wood <clay.wood@gmail.com>
2023-02-08 05:31:22 +0000

    Move hcunits into django project

--- hcunits/js/main_home.js
+++
@@ -1,7 +0,0 @@
-//const HOST_NAME = "https://api.hcunits.net"
-//var dataSource = new DataSource(HOST_NAME);
-var dataSource = new MockDataSource();
-
-var teamListView = new TeamListView(dataSource)
-
-drawHeaderAndFooter("")
---
+++ hcunits_api/hcunits_api/hcunits_api/serializers.py
@@ -0,0 +1,12 @@
+from hcunits_api.hcunits_api.models import Unit
+from rest_framework import serializers
+
+class UnitListSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Unit
+    fields = ['unit_id', 'set_id', 'name', 'collector_number', 'point_values', 'rarity']
+    
+class UnitDetailSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Unit
+    fields = '__all__'
